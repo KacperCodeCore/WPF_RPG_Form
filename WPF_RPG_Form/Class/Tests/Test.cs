97473b4f9df866267d8eb14c82e19b19bdfb9e9f
Kacper <silynazamiary@gmail.com>
2024-12-03 16:58:14 +0100

    xUnit fix

---
+++ WPF_RPG_Form/Class/Tests/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace P_Lista_3_formularz.Class.Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Assert.Equal(3, 3);
+            Console.WriteLine("Test Completed");
+        }
+
+    }
+}
